8075ea328e9bcc6e47deb3fed2a3c5cd2a459553
George Hunt <georgejhunt@gmail.com>
2015-11-01 13:23:42 -0500

    start moving towards a SD build on a non xo1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mksdcard
+mkxo10.sh -- script which accepts an os-builder image name (assumed to be in /root/olcp-os-builder/images/xo1/). Then it partitions an SD card, mounts it, and copies the lzma tarball. Other parts of this script appear to be oblolete (as is almost all of it -- since I've decided to start with a stock 13.2.5 olpc build)
+
+8gb -- this script tries to copy desired kiwix content to the SD card -- assumes that the content is on the same machine located at /root/content/zims
+
+xo-custom -- written by Jerry Vonau, incorporated into mktinycorexo  by James Cameron, perhaps useful in doing a chroot, and all of the assembly on a SD card on a larger machine.
---
+++ scripts/shrinkit.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# resize the xo SD card to minumum size and zip it to current directory
+# assume the /dev/sdb2 is partition to be shrunk
+
+DEVICE=/dev/sdb
+PART=${DEVICE}2
+BLOCK_SIZE=512
+ROOT_PARTITION_START_BLOCK=139264
+NUM_HEADS=16
+NUM_SECTORS_PER_TRACK=62
+CHKDEV=blkid|grep OLPCRoot|cut -d" " -f 1
+if [ "$CHKDEV" != "$PART" ];then
+   echo "OLPCRoot no at exected location"
+   exit 1
+fi
+
+# Automatically determine a size for the output disk image (including root
+# and boot partitions).
+#
+# This is calculated by using resize2fs to shrink, then adding the space
+# occupied by partition 1
+auto_size(){
+		  # fetch the size of /dev/sdb2
+		  blocks4k=`e2fsck -n $PART 2>/dev/null|grep blocks|cut -f5 -d" "|cut -d/ -f2`
+		  #pick up the ending sector of first partition
+		  sectors=`fdisk -l|grep ${DEVICE}1|gawk '{print $4}'`
+		  total_sectors=$(( $blocks4k * 8 + $sectors ))
+		  newsectors=$(( $blocks4k * 8 ))
+		  echo $newsectors
+}
+
+resize_image()
+* recieves parameter number of sectors in partition 2 or "auto"
+{
+	local disk_size=$1
+	if [ "$disk_size" = "auto" ]; then
+		part_size=$(auto_size)
+	fi
+
+	echo "Making partition of size $part_size"
+read -p "pause for verification" ans
+
+	/sbin/sfdisk -S 32 -H 32 --force -uS $DEVICE <<EOF
+8192,131072,83,*
+$ROOT_PARTITION_START_BLOCK,$part_size,,
+EOF
+}
+
+#auto_size /dev/sdb2
+e2fsck -f $PART
+resize2fs $PART
+resize_image auto
+e2fsck -f $PART
+read -p "what is filename for this image" FILENAME
+dd if=$DEVICE of=$FILENAME bs=512 count=$total_sectors
+zip $FILENAME.zip $FILENAME
