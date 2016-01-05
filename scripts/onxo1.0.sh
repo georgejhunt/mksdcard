b01677f03c61cdb0da398aa341d57673e0a847c6
George Hunt <georgejhunt@gmail.com>
2016-01-05 15:05:33 +0000

    correct the epiphany invokation

--- scripts/onxo1.0.sh
+++ scripts/onxo1.0.sh
@@ -48,7 +48,7 @@ cd $SCRIPTPATH
 chmod 4755 /usr/bin/xs-remote-on
 chmod 4755 /usr/bin/xs-remote-off
 su olpc -c 'gsettings set org.gnome.Epiphany restore-session-policy never'
-sed -i -e's|^Exec=.*|Exec=file:///library/index.html %U|' /usr/share/applications/epiphany.desktop
+sed -i -e's|^Exec=.*|Exec=/bin/epiphany file:///library/index.html %U|' /usr/share/applications/epiphany.desktop
 
 echo "all done"
 
